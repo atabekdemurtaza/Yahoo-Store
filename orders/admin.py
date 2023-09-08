9d4c10a4bd3372a85778497b0cc9c3f9873bed71
atabekdemurtaza <mychannel21war@gmail.com>
2023-09-08 17:35:12 +0500

    #9

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,48 @@
+import csv
+import datetime
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'Закрепить; тип файла={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+
+    # Записываем первую строку  с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+    # Записываем строк данных
+    for obj in queryset:
+        data_row = list()
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Вывести как CSV'
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f"<a href='{url}'>Просмотр</a>")
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f"<a href='{url}'>PDF</a>")
+
+
+order_pdf.short_description = 'Счет-фактура'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -18,10 +61,12 @@ class OrderAdmin(admin.ModelAdmin):
         'postal_code',
         'city',
         'created',
-        'updated'
+        'updated',
+        order_detail,
+        order_pdf
     ]
     list_filter = ['created', 'updated', 'paid']
     inlines = [
         OrderItemInline,
     ]
-
+    actions = [export_to_csv]
--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import order_create
+from .views import order_create, admin_order_detail, admin_order_pdf
 
 app_name = 'orders'
 
@@ -8,5 +8,15 @@ urlpatterns = [
         route='create/',
         view=order_create,
         name='order_create'
+    ),
+    path(
+        route='admin/order/<int:order_id>/',
+        view=admin_order_detail,
+        name='admin_order_detail'
+    ),
+    path(
+        route='admin/order/<int:order_id>/pdf/',
+        view=admin_order_pdf,
+        name='admin_order_pdf'
     ),
 ]
